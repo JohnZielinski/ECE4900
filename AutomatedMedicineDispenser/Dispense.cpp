e70ac468a3c9e5411c0fe33d21f4e8ab044b42a1
John <john.m.zie@gmail.com>
2018-04-17 16:12:06 -0400

    Changed dispense function

--- AutomatedMedicineDispenser/Dispense.cpp
+++ AutomatedMedicineDispenser/Dispense.cpp
@@ -4,13 +4,27 @@
 void dispense(Servo servo, int numPills){
   for (int i = 0; i < numPills; i++) {
     // Shake the gear at the bottom to make sure a pill is in
+    for (int j = 0; j < 5; j++){
+      servo.write(0);
+      delay(300);
+      servo.write(30);
+      delay(300);
+    }
     servo.write(0);
-    servo.write(15);
-    servo.write(0);
-    servo.write(15);
-    servo.write(0);
+    delay(500);
     // Take the pill to the top
     servo.write(180);
+    delay(1000);
+    // Below is for reading a lasor sensor, not used though
+    /*
+    unsigned long timeNow = millis();
+    unsigned long readForTime = 2000; // Read for 5 seconds
+    while(millis() - timeNow < readForTime){
+      if (digitalread(laser_pin == LOW){
+        Serial.println("Pill Detected");
+      }
+    }
+    */
   }
     servo.write(0);
 }
--- DispenseTest/DispenseTest.ino
+++ DispenseTest/DispenseTest.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-int servoPin = 3;
+int servoPin = 9;
 int numPills = 10;
 
 Servo servo;
@@ -22,13 +22,23 @@ void loop() {
 void dispense(Servo servo, int numPills){
   for (int i = 0; i < numPills; i++) {
     // Shake the gear at the bottom to make sure a pill is in
+    for (int j = 0; j < 5; j++){
+      servo.write(0);
+      delay(300);
+      servo.write(30);
+      delay(300);
+    }
     servo.write(0);
-    servo.write(15);
-    servo.write(0);
-    servo.write(15);
-    servo.write(0);
+    delay(500);
     // Take the pill to the top
     servo.write(180);
+    unsigned long timeNow = millis();
+    unsigned long readForTime = 2000; // Read for 5 seconds
+    while(millis() - timeNow < readForTime){
+//      if (digitalread(laser_pin == LOW){
+        //Serial.println("Pill Detected");
+      //}
+    }
   }
     servo.write(0);
 }
